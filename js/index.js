1cf352aaeeef25dd764c6438b5851e5e35e56ff3
CB6 <melissa.j.womack@gmail.com>
2018-08-22 21:29:29 -0500

    Lots of changes

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instance = M.Carousel.init({
+        fullWidth: true,
+        indicators: true
+      });
+  });
